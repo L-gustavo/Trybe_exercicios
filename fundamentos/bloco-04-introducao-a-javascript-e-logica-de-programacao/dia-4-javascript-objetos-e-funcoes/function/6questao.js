c511b8f87040bbe48d2f5b61b3ec13c00bfe9397
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-09-18 17:36:38 -0300

    reajustando

--- fundamentos/bloco-04-introducao-a-javascript-e-logica-de-programacao/dia-4-javascript-objetos-e-funcoes/function/6questao.js
+++
@@ -1,9 +0,0 @@
-// Crie uma função que receba um número inteiro N e retorne o somatório de todos os números de 1 até N
-function somaTodosNumeros(numeros) {
-    let total = 0;
-    for (let index = 1; index <= numeros; index += 1) {
-      total = total + index;
-    }
-    return total;
-  }
-  console.log(somaTodosNumeros(4));
