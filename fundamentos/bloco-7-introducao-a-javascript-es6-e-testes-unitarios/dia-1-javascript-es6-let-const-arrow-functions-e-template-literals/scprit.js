3c28efb05838ee4e523f25f568ccaf904d0c9a70
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-09-23 18:36:41 -0300

    questao 2 feita

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scprit.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scprit.js
@@ -1,13 +1,31 @@
 // 1 questão
-const testingScope = (escopo) => {
-  if (escopo === true) {
-    let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
-    ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
-    console.log(ifScope);
-  } else {
-     let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
-    console.log(elseScope);
-  }
+// const testingScope = (escopo) => {
+//   if (escopo === true) {
+//     let ifScope = 'Não devo ser utilizada fora do meu escopo (if)';
+//     ifScope = `${ifScope} ótimo, fui utilizada no escopo !`;
+//     console.log(ifScope);
+//   } else {
+//      let elseScope = 'Não devo ser utilizada fora meu escopo (else)';
+//     console.log(elseScope);
+//   }
+// }
+
+// testingScope(true);
+
+// 2 questão 
+const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+
+const sorteOddsAndEvens = () => {
+  oddsAndEvens[0] = 2
+  oddsAndEvens[1] = 3
+  oddsAndEvens[2] = 4
+  oddsAndEvens[3] = 7
+  oddsAndEvens[4] = 10
+  oddsAndEvens[5] = 13
+
+  return oddsAndEvens;
 }
 
-testingScope(true);
+  const pegaOrds = sorteOddsAndEvens();
+
+console.log(`Os numeros ${pegaOrds} se encontram ordenados de forma crescente!`); 
