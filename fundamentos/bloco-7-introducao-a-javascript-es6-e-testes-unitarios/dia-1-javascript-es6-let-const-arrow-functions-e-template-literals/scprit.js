66e6d7e26ddfadb6ac1d7bcde4de3908346639f5
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-09-28 18:51:27 -0300

    parte do exercicio 7.1

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scprit.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-1-javascript-es6-let-const-arrow-functions-e-template-literals/scprit.js
@@ -13,19 +13,27 @@
 // testingScope(true);
 
 // 2 questão 
-const oddsAndEvens = [13, 3, 4, 10, 7, 2];
+// const oddsAndEvens = [13, 3, 4, 10, 7, 2];
 
-const sorteOddsAndEvens = () => {
-  oddsAndEvens[0] = 2
-  oddsAndEvens[1] = 3
-  oddsAndEvens[2] = 4
-  oddsAndEvens[3] = 7
-  oddsAndEvens[4] = 10
-  oddsAndEvens[5] = 13
+// const sorteOddsAndEvens = () => {
+//   oddsAndEvens[0] = 2
+//   oddsAndEvens[1] = 3
+//   oddsAndEvens[2] = 4
+//   oddsAndEvens[3] = 7
+//   oddsAndEvens[4] = 10
+//   oddsAndEvens[5] = 13
 
-  return oddsAndEvens;
-}
+//   return oddsAndEvens;
+// }
 
-  const pegaOrds = sorteOddsAndEvens();
+//   const pegaOrds = sorteOddsAndEvens();
 
-console.log(`Os numeros ${pegaOrds} se encontram ordenados de forma crescente!`); 
+// console.log(`Os numeros ${pegaOrds} se encontram ordenados de forma crescente!`); 
+
+//  Parte 2 do dia 7.1 
+
+// 1 questão
+
+const fatorial = (num) => (num<2)? 1:fac(num-1)*num;
+
+console.log(fatorial(num(4)));
