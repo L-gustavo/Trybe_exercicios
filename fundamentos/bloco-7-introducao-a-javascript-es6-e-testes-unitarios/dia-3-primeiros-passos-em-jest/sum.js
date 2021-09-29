00eafd532d830630f7ad96f73725430435798191
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-09-29 14:01:42 -0300

    exercicio feito ate 6

--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.js
@@ -26,5 +26,59 @@ function myFizzBuzz(num) {
   return num;
 }
 
+// 4 questão
+function encode(vogaisEmNumeros) {
+  vogaisEmNumeros = vogaisEmNumeros.replace(/a/g, '1');
+  vogaisEmNumeros = vogaisEmNumeros.replace(/e/g, '2');
+  vogaisEmNumeros = vogaisEmNumeros.replace(/i/g, '3');
+  vogaisEmNumeros = vogaisEmNumeros.replace(/o/g, '4');
+  vogaisEmNumeros = vogaisEmNumeros.replace(/u/g, '5');
+  return vogaisEmNumeros;
+  }
+  
+  function decode(numeroEmVogais) {
+  numeroEmVogais = numeroEmVogais.replace(/1/g, 'a');
+  numeroEmVogais = numeroEmVogais.replace(/2/g, 'e');
+  numeroEmVogais = numeroEmVogais.replace(/3/g, 'i');
+  numeroEmVogais = numeroEmVogais.replace(/4/g, 'o');
+  numeroEmVogais = numeroEmVogais.replace(/5/g, 'u');
+  return numeroEmVogais; 
+  }
 
-module.exports = {sum, myRemove, myFizzBuzz};
+  function techList(array, nome) {
+    // seu código aqui
+    array.sort();
+    let techs = [];
+    if (array.length === 0) {
+      return "Vazio!";
+    }
+    // for pra fazer entra em todo array
+    for (let value of array) {
+      techs.push({tech: value, name: nome});
+    }
+    return techs;
+  }
+
+  function hydrate(phrase) {
+    // forma de encontrar numeros em uma string - pesquisa: YoutubeJavaScript Problem: Extracting Numbers from a String.
+    let reg = /\d+/g;
+    let result = phrase.match(reg);
+    // percorrer o array formado para transformar em number
+    for (let index in result) {
+    result[index] = parseInt(result[index], 10);
+    }
+    // fazer a soma dos numeros do array criado
+    let sum = 0;
+    for (let index = 0; index < result.length; index += 1) {
+    sum += result[index];
+    }
+    // condição para verricar quantos copos de água terão.
+    if (sum > 1) {
+    let string1 = ' copos de água';
+    return sum + string1;
+    }
+    let string2 = ' copo de água';
+    return sum + string2;
+  };
+
+module.exports = {sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate};
--- fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
+++ fundamentos/bloco-7-introducao-a-javascript-es6-e-testes-unitarios/dia-3-primeiros-passos-em-jest/sum.test.js
@@ -1,5 +1,4 @@
-const { expect } = require('@jest/globals');
-const {sum, myRemove, myFizzBuzz} = require('./sum');
+const {sum, myRemove, myFizzBuzz, encode, decode, techList, hydrate} = require('./sum');
 
 describe('Function sum', () => {
   it('sums two values', () => {
@@ -45,4 +44,80 @@ describe('function myFizzBuzz', () => {
   it('Faça uma chamada com um parâmetro que não é um número e verifique se o retorno é o esperado', () => {
     expect(myFizzBuzz('2')).toBe(false)
   })
+});
+
+describe('Functions encode e decode', () => {
+  it('Teste se encode é função', () => {
+    expect(typeof encode).toEqual('function')
+  })
+  it('Teste se decode é função', () => {
+    expect(typeof decode).toEqual('function');
+  })
+  it('Para a função encode teste se as vogais a, e, i, o, u são convertidas em 1, 2, 3, 4 e 5, respectivamente', () => {
+    expect(encode('a, e, i, o, u')).toBe('1, 2, 3, 4, 5');
+  })
+  it('Para a função decode teste se as vogais 1, 2, 3, 4 e 5 são convertidas em a, e, i, o, u, respectivamente', () => {
+    expect(decode('1, 2, 3, 4, 5')).toBe('a, e, i, o, u');
+  })
+  it('Teste se as demais letras/números não são convertidos para cada caso', () => {
+    expect(encode('b, c, d, f')).toBe('b, c, d, f');
+  })
+  it('Teste se as demais letras/números não são convertidos para cada caso', () => {
+    expect(decode('b, c, d, f')).toBe('b, c, d, f');
+  })
+  it('Teste se a string que é retornada pelas funções têm o mesmo número de caracteres que a string passada como parâmetro', () => {
+    expect(encode('gustavo').length).toBe(7);
+  })
+});
+
+describe('Testa a função techList', () => {
+  it('Testa se a função techList é definida', () => {
+    expect(techList).toBeDefined();
+  });
+  it('Testa se techList é uma função', () => {
+    expect(typeof techList).toBe('function');
+  });
+  it('Lista com 5 tecnologias deve retornar uma lista de objetos ordenados', () => {
+    expect(techList(['React', 'Jest', 'HTML', 'CSS', 'JavaScript'], 'Lucas')).toEqual([
+      {
+        tech: 'CSS',
+        name: 'Lucas'
+      },
+      {
+        tech: 'HTML',
+        name: 'Lucas'
+      },
+      {
+        tech: 'JavaScript',
+        name: 'Lucas'
+      },
+      {
+        tech: 'Jest',
+        name: 'Lucas'
+      },
+      {
+        tech: 'React',
+        name: 'Lucas'
+      }
+    ]);
+  });
+  it('Lista com 0 tecnologias deve retornar uma mensagem de erro "Vazio!"', () => {
+    expect(techList([], 'Lucas')).toBe('Vazio!');
+  });
+});
+
+describe('Testa a função hydrate', () => {
+  it('Testa se a função hydrate é definida', () => {
+    expect(hydrate).toBeDefined();
+  });
+  it('Testa se hydrate é uma função', () => {
+    expect(typeof hydrate).toBe('function');
+  });
+  it('Ao receber uma string retorne a sugestão de quantos copos de água deve-se beber', () => {
+    expect(hydrate('1 cerveja')).toBe('1 copo de água');
+    expect(hydrate('1 cachaça, 5 cervejas e 1 copo de vinho')).toBe('7 copos de água');
+    expect(hydrate('2 shots de tequila, 2 cervejas e 1 corote')).toBe('5 copos de água');
+    expect(hydrate('1 copo de catuaba, 1 cervejas e 1 copo de vinho')).toBe('3 copos de água');
+    expect(hydrate('4 caipirinhas e 2 cervejas')).toBe('6 copos de água');
+  });
 });
