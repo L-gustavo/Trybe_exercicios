d04a479f0e0ae127f2eddc0c1d42c4b2605f8cca
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-10-08 20:42:48 -0300

    8 questao feita, finalizando

---
+++ fundamentos/bloco-8-higher-order-functions-do-javascript-es6/dia-5-javascript-es6-spread-operator-parametro-rest-destructuring-e-mais/exercise8.js
@@ -0,0 +1,9 @@
+// Escreva uma função greet que, dado o nome de uma pessoa, retorna uma mensagem de cumprimento:
+
+// escreva greet abaixo
+
+const greet = (name, cumprimento = 'Hi') => `${cumprimento} ${name}`;
+
+console.log(greet('John')) // 'Hi John'
+console.log(greet('John', 'Good morning')) // 'Good morning John'
+console.log(greet('Isabela', 'Oi')) // 'Oi Isabela'
