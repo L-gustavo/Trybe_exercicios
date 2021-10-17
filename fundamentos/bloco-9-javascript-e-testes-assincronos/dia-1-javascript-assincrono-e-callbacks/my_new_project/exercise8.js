7b9915065a02a2af4ab153f07f0879140e51f58d
Luis Gustavo <l.gustavomelo@hotmail.com>
2021-10-17 19:46:05 -0300

    9 feita e finalizado exercicio

--- fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/my_new_project/exercise8.js
+++ fundamentos/bloco-9-javascript-e-testes-assincronos/dia-1-javascript-assincrono-e-callbacks/my_new_project/exercise8.js
@@ -20,13 +20,13 @@ const pokemons = [
 function getPokemonDetails(filter, callback) {
   setTimeout(() => {
     if (pokemons.find(filter) === undefined) {
-      return callback(new Error('Não temos esse pokemon para você :('), null);
+      return callback(new Error('Não temos esse pokémon para você :('), null);
     }
     const pokemon = pokemons.find(filter);
 
     const { name, type, ability } = pokemon;
 
-    const messageFromProfOak = `Olá, seu pokemon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
+    const messageFromProfOak = `Olá, seu pokémon é o ${name}, o tipo dele é ${type} e a habilidade principal dele é ${ability}`;
 
     callback(null, messageFromProfOak);
   }, 2000);
